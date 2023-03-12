a300371a513b786e556159e143f92ccb3e4870ce
Alexandr Krasnov <120586418+AlexandrBeckers@users.noreply.github.com>
2023-03-12 20:56:06 +0400

    Initial commit

---
+++ settings.py
@@ -0,0 +1,26 @@
+class Settings:
+    chrome_driver = "/Users/alexandrkrasnov/PycharmProjects/Module_28.1/chromedriver"
+
+    base_url = 'https://b2c.passport.rt.ru'
+    valid_email = 'mr.beckers@bk.ru'
+    valid_password = 'Beckers589172'
+    valid_email_reg = 'mr.beckers@yandex.ru'
+    invalid_email = 'kavabanga@gmail.com'
+    invalid_password = 'wrongpassworD0'
+    empty_email = ''
+    empty_password = ''
+    menu_type_auth = ['Телефон', 'Почта', 'Логин', 'Лицевой счёт']
+    placeholder_name = ['Мобильный телефон', 'Электронная почта', 'Логин', 'Лицевой счёт']
+    first_name = 'First'
+    last_name = 'Last'
+    first_name_ = '-'
+    last_name_ = '-'
+    russian_generate_string = 'ъ'
+    latin_generate_string = 'string'
+    chinese_chars = '形声字/形聲字象形字'
+    special_chars = '!@#$%^&*()_+=/{}'
+    numbers = 274
+    empty = ''
+    passw1 = 'zXc12345'
+    passw2 = 'zXc12345vBn09Qw'
+    passw3 = 'ZaQwsX987654321Ujb42'
